c25eb246f77d35b334dcf605f6ee1253fea33486
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-07-10 17:13:19 +0900

    Update nsx.py

--- vmcjp/nsx.py
+++ vmcjp/nsx.py
@@ -116,9 +116,6 @@ class NetworkConfig(Config):
             {
                 "create_user": segment.get_field("create_user"),
                 "display_name": segment.get_field("display_name"),
-                "domain_name": segment.get_field("domain_name"),
-                "l2_extension": segment.get_field("l2_extension"),
-                "subnet": segment.get_field("subnets")[0].to_dict(),
                 "type": segment.get_field("type")
             }
             for segment in segments 
