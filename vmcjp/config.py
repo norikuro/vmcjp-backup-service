6215e76a6c905e77c4a8f4baad689ea22d8f9aef
EC2 Default User <ec2-user@ip-172-30-10-222.ap-northeast-1.compute.internal>
2019-07-10 07:25:26 +0000

    updated

---
+++ vmcjp/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import json
+
+from vmcjp.utils import dbutils
+from vmcjp.utils import constant
+from vmcjp.utils.s3utils import read_json_from_s3
+
+class Config(object):
+    def __init__(self):
+      f = json.load(open(constant.S3_CONFIG, "r"))
+      j = read_json_from_s3(f["bucket"], f["config"])
+      self.token = j["token"]
+      self.org_id = j["org_id"]
+      self.sddc_id = j["sddc_id"]
+      self.db = dbutils.DocmentDb(constant.S3_CONFIG, constant.SDDC_DB, constant.SDDC_COLLECTION)
